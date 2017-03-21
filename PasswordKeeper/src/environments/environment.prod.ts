4653e73574ac0e5a7ff41cfc0483dd148f580b98
Donald Sisco <dododonnars@gmail.com>
2017-03-21 08:33:52 -0400

    Initial Commit

---
+++ PasswordKeeper/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC6_JAQeNrz0waPCfta0cI_4aq81fy6ywU",
+    authDomain: "siscodr-passwordkeeper.firebaseapp.com",
+    databaseURL: "https://siscodr-passwordkeeper.firebaseio.com",
+    storageBucket: "siscodr-passwordkeeper.appspot.com",
+    messagingSenderId: "740396346316"
+  }
+};
