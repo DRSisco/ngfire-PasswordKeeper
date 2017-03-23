3f76ccf252bdfd15d7e03c6d01573b9ec4ee3f34
Donald Sisco <dododonnars@gmail.com>
2017-03-23 09:08:20 -0400

    Passwords?

--- PasswordKeeper/src/app/+main/main.component.ts
+++ PasswordKeeper/src/app/+main/main.component.ts
@@ -1,7 +1,14 @@
+import { Password } from './../password.model';
 import { Router } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFire, FirebaseAuthState } from "angularfire2";
+import { AngularFire, FirebaseAuthState, FirebaseListObservable } from "angularfire2";
 import { Subscription } from "rxjs/Subscription";
+
+// interface Password {
+//   service: string
+//   username?: string
+//   password: string
+// }
 
 @Component({
   selector: 'app-main',
@@ -9,7 +16,7 @@ import { Subscription } from "rxjs/Subscription";
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit, OnDestroy {
-    
+    private passwordStream: FirebaseListObservable<Password[]>
     private subscription: Subscription
     
     ngOnDestroy(): void {
@@ -18,9 +25,13 @@ export class MainComponent implements OnInit, OnDestroy {
 
 
   constructor(private af: AngularFire, private router: Router) { 
+
+
     this.subscription = af.auth.subscribe( (auth: FirebaseAuthState) => {
       if (auth) {
         console.log("You are logged in. All is good!")
+        var firebasePath = `/users/${auth.uid}`
+        this.passwordStream = this.af.database.list(firebasePath)
       } else {
         console.log("You are not logged in, bounce to Sign In page")
         this.router.navigate(['/signin']);
