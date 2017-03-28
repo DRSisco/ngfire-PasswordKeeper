8d838c52fffdfcd7ec30b666e82d6f252c6f1917
Donald Sisco <dododonnars@gmail.com>
2017-03-28 08:53:30 -0400

    css refactor

--- PasswordKeeper/src/app/password-display-card/password-display-card.component.ts
+++ PasswordKeeper/src/app/password-display-card/password-display-card.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Password } from "../password.model";
 
 @Component({
   selector: 'app-password-display-card',
   templateUrl: './password-display-card.component.html',
-  styleUrls: ['./password-display-card.component.css']
+  styleUrls: ['./password-display-card.component.scss', '../shared/common.scss']
 })
 export class PasswordDisplayCardComponent implements OnInit {
+
+  @Input() password: Password;
 
   constructor() { }
 
